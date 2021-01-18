d66cd64fd7a90dd337a88a7f5a80d8ff949f3193
= <=>
2021-01-18 15:28:03 +0200

    Creating calculations about dmg from Player and Monster

---
+++ app.js
@@ -0,0 +1,29 @@
+function damageFormula(max , min){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data(){
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        }
+    },
+    methods: {
+        attackMonster(){
+            //create attackValue with max and min for damage formula
+           const attackValue = damageFormula(12, 5);
+            // After attack monster health will be decreased
+           this.monsterHealth -= attackValue;
+            // And after this attack monster hit too so our health will be decreased too
+           this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue = damageFormula(15, 8);
+
+            this.playerHealth -= attackValue;
+        }
+    }
+});
+
+app.mount('#game');
